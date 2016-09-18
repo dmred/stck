91b8a568a2b4ab719b51921b8369d2527be65e37
dmred <dmredchenko@gmail.com>
2016-09-18 17:26:37 +0300

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+using std::size_t;
+using std::ostream;
 
 
 template <typename T>
@@ -12,11 +14,12 @@ class stack
 {
 public:
 	stack();
-	size_t count() const;
-	void push(T const &);
-	T pop();
-	stack(const stack<T>&);
-	stack<T>& operator=(const stack<T>&);
+	stack(stack const & stck);
+	auto count() const noexcept ->size_t;
+	auto push(T const & value)->void;
+	auto top() const->const T&;
+	auto pop()->void;
+	auto operator=(stack const & stck)->stack &;
 	~stack();
 private:
 	T * _array;
@@ -33,45 +36,49 @@ stack<T>::~stack() {
 }
 
 template <typename T>
-stack<T>::stack(const stack<T>& x) : _array_size(x._array_size), count_(x._count){
+stack<T>::stack(stack const &stck) : _array_size(stck._array_size), count_(stck._count) {
 	_array = new T[_array_size];
-	for (int i = 0; i < _count; i++) _array[i] = x._array[i];
+	for (int i = 0; i < _count; i++) _array[i] = stck._array[i];
 }
 
 template<typename T>
-stack<T>& stack<T>::operator=(const stack<T>& b){
-	if (this != &b){
-		swap(b._array_size,_array_size);
-		swap(b._count, _count);
-		swap(b._array, _array);
-
+ auto stack<T>::operator=(stack const & stck) -> stack & {
+	if (this != &stck) {
+		(stack(stck)).swap(*this);
 	}
-return *this;
-}
-template <typename T>
-size_t stack<T>::count() const { return _count; }
-
-template <typename T>
-void stack<T>::push(T const &a) {
-	if (_array == nullptr) {
-		_array = new T[1]; _array[0] = a;
-		_count++; _array_size++;
-	}
-	else {
-		if (_count == _array_size) {
-			T *p = _array;
-			_array = new T[_array_size * 2];
-			for (int i = 0; i < _count; i++) _array[i] = p[i];
-			delete[]p;
-			_array_size *= 2;
-		}
-		_array[_count] = a;
-		_count++;
-	}
+	return *this;
 }
 
 template <typename T>
-T stack<T>::pop() {
+auto stack<T>::count() const noexcept->size_t { return _count; }
+
+template<typename T>
+ auto stack<T>::push(T const & value)->void {
+	if (_count == _array_size)
+	{
+		_array_size *= 2;
+		T * time = new T[array_size_];
+		copy(_array, _array_size + _array, time
+		);
+		delete[] _array;
+
+		array_ = time;
+		delete[] time;
+	}
+	_array[_count] = value;
+	_count++;
+
+}
+
+ template <typename T>
+ auto stack<T>::top() const -> const T&{
+	 if (_count == 0) throw logic_error("stack's empty");
+	 return _array[_count - 1];
+ }
+
+
+template <typename T>
+auto stack<T>::pop() -> void {
 	if (_count == 0) throw("stack's empty");
 	else {
 		_count = _count - 1;
