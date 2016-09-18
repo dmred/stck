82332b0a2fa38549ec7be89f6b1451fc892ad78b
dmred <dmredchenko@gmail.com>
2016-09-18 20:43:04 +0300

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -20,6 +20,6 @@ private:
 	T * _array;
 	size_t _array_size;
 	size_t _count;
-	auto m_c(size_t count_m_с, size_t array_size_m_с, const T * tmp)->T*;
+	auto m_c(size_t count_m_c, size_t array_size_m_c, const T * tmp)->T*;
 };
 
